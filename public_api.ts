e130ef27a9bcb0243b5c0262db8c4b27f83348eb
Paul Ryan <paul@simplycomplex.co>
2017-12-06 17:52:32 -0500

    Edit .gitignore to publish

--- public_api.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/virtual-scroll';
--- src/virtual-scroll.ts
+++
@@ -1,3 +0,0 @@
-export { VirtualScrollComponent, ChangeEvent } from './virtual-scroll.component';
-export { VirtualScrollModule } from './virtual-scroll.module';
-export { WindowRef } from './window-ref';
